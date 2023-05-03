200119b429ed4a6820facb05397c1411ae1075b8
Javier Martín Lozano <javimartinlozano@hotmail.com>
2023-05-03 10:21:25 +0200

    Initialization

---
+++ src/main/java/com/inditex/productvisibility/ProductVisibilityApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.productvisibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductVisibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductVisibilityApplication.class, args);
+	}
+
+}
